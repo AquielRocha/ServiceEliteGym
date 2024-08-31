ffd5f5bf8f83540dae3e6663b113e45a2aa2ea60
aquiel <restefino1@gmail.com>
2024-08-31 20:28:00 -0300

    feat:new

---
+++ Dtos/Alunos/EnderecoDto.cs
@@ -0,0 +1,12 @@
+public class EnderecoDto
+{
+    public int Id { get; set; }
+    public string Rua { get; set; }
+    public string Numero { get; set; }
+    public string Complemento { get; set; }
+    public string Bairro { get; set; }
+    public string Cidade { get; set; }
+    public string Estado { get; set; }
+    public string CodigoPostal { get; set; }
+    public string Pais { get; set; }
+}
---
+++ Dtos/Alunos/ReadAlunoDto.cs
@@ -0,0 +1,28 @@
+using SERVICE.Dtos.Enderecos;
+
+namespace SERVICE.Dtos.Alunos
+{
+    public class ReadAlunoDto{
+
+ public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public string FotoBase64 { get; set; }
+    public string Tipo { get; set; }
+    public DateTime DataNascimento { get; set; }
+    public string Telefone { get; set; }
+    public DateTime DataCadastro { get; set; }
+    public string Objetivos { get; set; }
+    public string TipoPlano { get; set; }
+    public string StatusPagamento { get; set; }
+    public string InformacoesMedicas { get; set; }
+    public string PreferenciasTreino { get; set; }
+    public bool Ativo { get; set; }
+    public List<EnderecoDto> Enderecos { get; set; } // Lista de endereços
+
+
+    }
+
+
+
+}
